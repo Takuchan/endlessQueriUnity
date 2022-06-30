38d2e26b32b8aff8478186f7a20153eeb25497ae
Takuchan <takuchanapp@gmail.com>
2022-07-01 00:43:20 +0900

    Swift対応

---
+++ Debug/Query/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Query
+//
+//  Created by 松村拓洋 on 2022/06/22.
+//  Copyright © 2022 Unity Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
